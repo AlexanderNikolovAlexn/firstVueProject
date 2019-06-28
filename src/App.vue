0fa7a20b4d99c76811d6ee7147a0c87f22b7199e
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2019-06-28 21:08:18 +0300

    Initial project setup for Vue with Materialize css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavBar/>
+    <h2>App</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/Navbar";
+
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    NavBar
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Vue Tutorial</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
