a3d7f08f89c108bf4ec2752511b6b23e4547e0a4
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2019-06-29 10:32:39 +0300

    Add home vue -> list of posts

---
+++ src/components/PostForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form v-if="!loading" class="form" v-on:submit="onSubmit">
+    <div class="input-field">
+      <label for="title">Title</label>
+      <input type="text" name="title" v-model="title" class="validate">
+      <span class="helper-text" data-error="Title must not be empty"></span>
+    </div>
+    <div class="input-field">
+      <label for="body">Body</label>
+      <input type="text" name="body" v-model="body" class="validate">
+      <span class="helper-text" data-error="Body must not be empty"></span>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
+
